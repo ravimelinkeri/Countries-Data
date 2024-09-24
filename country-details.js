ec4ba1a1d055abe456ad8efe80719d643a8a3a11
Ravi Melinkeri <ravimelinkeribs@gmail.com>
2024-09-24 21:42:00 +0530

    Add files via upload

---
+++ country-details.js
@@ -0,0 +1,98 @@
+
+const imgFlag = document.querySelector('.flag')
+const titleHeading = document.querySelector('.title-heading')
+const nativeName = document.querySelector('.Native')
+const Population = document.querySelector('.Population')
+const Region = document.querySelector('.Region')
+const subRegion = document.querySelector('.Sub-Region')
+const Capital = document.querySelector('.Capital')
+const Domain = document.querySelector('.Domain')
+const Currencies = document.querySelector('.Currencies')
+const Languages = document.querySelector('.Languages')
+const borderr = document.querySelector('.borderr')
+const btn = document.querySelector('.btn')
+const light = document.querySelector('#light')
+const dark = document.querySelector('#dark')
+
+// Mode changer
+
+const modeChange = document.querySelector('.mode-change')
+const darkChange = document.querySelector('.dark')
+
+const countryName = new URLSearchParams(location.search).get('name')
+// console.log(countryName);
+
+fetch(`https://restcountries.com/v3.1/name/${countryName}?fullText=true`).then((res) => res.json())
+.then(([country]) =>{
+
+    // console.log(country);
+
+    // console.log((Object.values(country.currencies)));
+
+    // console.log(country.tld.join(', '));
+
+    imgFlag.src = country.flags.svg
+
+    titleHeading.innerText = country.name.common
+
+    Population.innerText = country.population.toLocaleString('en-US')
+
+    Region.innerText = country.region
+
+    if(country.subregion){
+        subRegion.innerText = country.subregion
+    }
+
+    if(country.capital){
+        Capital.innerText = country.capital
+    }
+
+
+    if(country.name.nativeName){
+        nativeName.innerText = (Object.values(country.name.nativeName)[0].common);
+    } else{
+        nativeName.innerText = country.name.common
+    }
+
+    if(country.tld){
+        Domain.innerText = (country.tld.join(', '));
+    }
+
+    if(country.currencies){
+        Currencies.innerText = ((Object.values(country.currencies).map((currency) => currency.name).join(',')));
+   }
+
+    if(country.languages){
+        Languages.innerText = ((Object.values(country.languages).join(', ')));
+   }
+
+    if(country.borders) {
+        country.borders.forEach((border)=>{
+            fetch(`https://restcountries.com/v3.1/alpha/${border}`)
+            .then((res)=> res.json())
+            .then(([borderCountry]) => {
+            const borderCountryTag = document.createElement('a')
+            borderCountryTag.classList.add('border-details')
+            borderCountryTag.href = `country-details.html?name=${borderCountry.name.common}`
+            borderCountryTag.innerText = borderCountry.name.common
+            // console.log(borderCountryTag);
+
+            borderr.append(borderCountryTag)
+            
+                
+        })
+        })
+    }
+
+})
+
+
+
+modeChange.addEventListener('click', ()=>{
+    darkChange.classList.toggle("dark");
+    light.style.display = "block"
+    if(light.style.display === "block"){
+        dark.style.display = "none"
+    } 
+})
+
---
+++ index.js
@@ -0,0 +1,76 @@
+
+const countryContainer =document.querySelector('.country-container')
+const modeChange = document.querySelector('.mode-change')
+const darkChange = document.querySelector('.dark')
+const icon = document.querySelector('#icon')
+
+const light = document.querySelector('#light')
+const dark = document.querySelector('#dark')
+
+const filterbyregion = document.querySelector('#filter-by-region')
+const Seaechcontainer = document.querySelector('.searchbar input')
+
+let allCountriesData
+
+
+fetch('https://restcountries.com/v3.1/all')
+.then((res) => res.json())
+.then((data) =>{
+    renderCountries(data)
+    allCountriesData = data
+})
+
+filterbyregion.addEventListener('change', (e)=>{
+    fetch(`https://restcountries.com/v3.1/region/${filterbyregion.value}`)
+    .then((res) => res.json())
+    .then(renderCountries)
+})
+
+function renderCountries(data){
+    countryContainer.innerHTML = ''
+    data.forEach((country) =>{
+
+        // console.log(country.borders)
+       
+        const countryCard = document.createElement('a')
+        countryCard.classList.add('country-card')
+        countryCard.href=`/country-details.html?name=${country.name.common}`
+        
+        countryCard.innerHTML = `
+            <img src="${country.flags.svg}" alt="Flag Img" class="country-flag">
+                    <h3 class="country-name">${country.name.common}</h3>
+                    <div class="country-details">
+                        <p><span>Population :</span> ${country.population.toLocaleString('en-US')}</p>
+                        <p><span>Region :</span> ${country.region}</p>
+                        <p><span>Capital :</span> ${country.capital}</p>
+                    </div>
+        
+        `
+        
+        countryContainer.append(countryCard) 
+
+    })
+}
+
+Seaechcontainer.addEventListener('input', (e)=>{
+    // console.log(e.target.value);
+    // console.log(allCountriesData);
+    const filteredCountry = allCountriesData.filter((country) => country.name.common.toLowerCase().includes(e.target.value.toLowerCase()))
+    renderCountries(filteredCountry)
+})
+
+modeChange.addEventListener('click', ()=>{
+    darkChange.classList.toggle("dark");
+    light.style.display = "block"
+    if(light.style.display === "block"){
+        dark.style.display = "none"
+    } 
+})
+
+
+
+
+
+
+
+
